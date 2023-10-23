4470bdb32f8886a94706977fb85a96a98596b03a
cris <cris28caldeira@gmail.com>
2023-10-24 00:29:34 +0100

    adding all files made in the course

---
+++ src/robots.js
@@ -0,0 +1,75 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'W3Schools',
+		username: 'w3',
+		email: 'w3schools.com'
+	},
+	{
+		id: 2,
+		name: 'Geeks for Geeks',
+		username: 'gg',
+		email: 'geeksforgeeks.org'
+	},
+	{
+		id: 3,
+		name: 'Free Code Camp',
+		username: 'fcc',
+		email: 'freecodecamp.org'
+	},
+	{
+		id: 4,
+		name: 'MDN Web Docs',
+		username: 'mdn',
+		email: 'developer.mozilla.org'
+	},
+	{
+		id: 5,
+		name: 'CSS-Tricks',
+		username: 'css-t',
+		email: 'css-tricks.com/almanac/'
+	},
+	{
+		id: 6,
+		name: 'Bootstrap',
+		username: 'bootstrap',
+		email: 'https://getbootstrap.com'
+	},
+	{
+		id: 7,
+		name: 'JS Array Explorer',
+		username: 'array-explorer',
+		email: 'arrayexplorer.netlify.app'
+	},
+	{
+		id: 8,
+		name: 'JS Object Explorer',
+		username: 'object-explorer',
+		email: 'objectexplorer.netlify.app'
+	},
+	{
+		id: 9,
+		name: 'Animate.css',
+		username: 'animate.css',
+		email: 'animate.style/#usage'
+	},
+	{
+		id: 10,
+		name: 'Can I use...?',
+		username: 'caniuse',
+		email: 'caniuse.com'
+	},
+	{
+		id: 11,
+		name: 'Babel',
+		username: 'babel',
+		email: 'babeljs.io'
+	},
+	{
+		id: 12,
+		name: 'Google Fonts',
+		username: 'google-fonts',
+		email: 'fonts.google.com'
+	},
+	
+	]
